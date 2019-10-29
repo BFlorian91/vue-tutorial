9ab1077c46ef8189c220b5c5f30206cc6b831b0e
Florian <flbeaumo@student.42.fr>
2019-10-29 19:07:07 +0100

    42

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -2,6 +2,8 @@
   <div>
     <h1> Titre 1 </h1>
     <h2> Titre 2 </h2>
+      <!-- <button @click="count += 1">add</button> -->
+      <!-- <p>The button has been click {{ count }}.</p> -->
   </div>
 </template>
 
